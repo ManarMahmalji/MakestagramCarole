fc0e34a01287c1562e9c6a9f5ca657765814eb48
ManarMahmalji <31004981+ManarMahmalji@users.noreply.github.com>
2017-08-17 13:52:25 +0300

    Finished step 7

--- Makestagram/Controllers/LoginViewController.swift
+++ Makestagram/Controllers/LoginViewController.swift
@@ -6,6 +6,11 @@
 //  Copyright © 2017 Manar Mahmalji. All rights reserved.
 //
 import UIKit
+import FirebaseAuth
+import FirebaseAuthUI
+
+typealias FIRUser = FirebaseAuth.User
+
 class LoginViewController: UIViewController{
     
     @IBOutlet weak var loginButton: UIButton!
@@ -20,6 +25,24 @@ class LoginViewController: UIViewController{
     
     @IBAction func loginButtonTapped(_ sender: UIButton) {
         print("login Button Tapped")
+        guard let authUI = FUIAuth.defaultAuthUI()
+            else{ return }
+        authUI.delegate = self
+        let authViewController = authUI.authViewController()
+        present(authViewController, animated: true)
+    }
+    
+}
+
+extension LoginViewController: FUIAuthDelegate{
+    func authUI(_ authUI: FUIAuth, didSignInWith user: FIRUser?, error: Error?) {
+        if let error = error {
+            assertionFailure("Error signing in: \(error.localizedDescription)")
+    }
+        
+      print("Handle user sign up / log in")
+    
     }
 }
+
 
